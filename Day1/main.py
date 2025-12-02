83d5ad3645f974e2eaecc8bfd43f71a1e2bbd611
YaroslavWork <yarik0980504744@gmail.com>
2025-12-02 14:59:38 +0100

    complete day1 part2

--- Day1/main.py
+++ Day1/main.py
@@ -4,8 +4,21 @@ def move_dial(is_left: bool, to: int, start_position: int) -> int:
     else:
         return (start_position + to) % 100
 
-def is_pointing_to_zero(dial_position: int) -> bool:
-    return dial_position == 0
+def check_how_many_times_dial_pointing_at_zero(
+        is_left: bool,
+        to: int,
+        start_position: int
+    ) -> int:
+
+    if is_left:
+        # To include zero must do operation -1
+        result = abs((start_position - to - 1) // 100)
+        if start_position == 0:
+            return result - 1 # We now on zero (it is does not count)
+        return result
+    else:
+        return (start_position + to) // 100
+
 
 if __name__ == '__main__':
     # First parameter tell if dial is moving left than it's true
@@ -21,13 +34,16 @@ if __name__ == '__main__':
     dial_position = 50
     pointing_to_zero_counts = 0
     for combination in combinations:
-        dial_position = move_dial(
+        new_dial_position = move_dial(
             is_left=combination[0],
             to=combination[1],
             start_position=dial_position
         )
-        pointing_to_zero_counts += is_pointing_to_zero(
-            dial_position=dial_position
-        ) # add one to variable if function is true (dial points to zero)
+        pointing_to_zero_counts += check_how_many_times_dial_pointing_at_zero(
+            is_left=combination[0],
+            to=combination[1],
+            start_position=dial_position
+        )
+        dial_position = new_dial_position
     
     print(pointing_to_zero_counts)
