ecebe9f1ba74a508823b3a3479809ec3881cbe4a
YaroslavWork <yarik0980504744@gmail.com>
2025-12-04 20:44:59 +0100

    complete day2 part2 (main function doesn't work but brute force is ;)

--- Day2/main.py
+++ Day2/main.py
@@ -1,20 +1,64 @@
 def find_pattern_between_ranges(begin: int, end: int) -> list[int]:
-    # Find first half of begin and end numbers
-    if begin < 10:
-        half_of_begin_number = 0
-    else:
-        half_of_begin_number = int(str(begin)[:int(len(str(begin)) // 2)])
-    half_of_end_number = int(str(end)[:len(str(begin)) // 2 + 1])
-    
+    raise NotImplementedError('Function not pass unit tests.')
     pattern_numbers = []
-    for i in range(half_of_begin_number, half_of_end_number+1):
-        #Check if repeated number is between range
-        number = int(str(i)+str(i))
-        if begin <= number <= end:
+    max_length_of_half_number = len(str(end)) // 2 + 1
+    min_number_length = len(str(begin))
+    max_number_length = len(str(end))
+
+    for current_length in range(1, max_length_of_half_number + 1):
+        start_number = int(str(begin)[:current_length])-1
+        end_number = int(str(end)[:current_length])+1
+        if end_number < start_number:
+            end_number = int(str(end)[:current_length + 1])
+
+        for current_range in range(start_number, end_number + 1):
+            #Check if repeated number is between range
+            for repeats in range(
+                min_number_length//current_length-1,
+                max_number_length//current_length+1
+            ):
+                if repeats <= 0:
+                    continue
+                
+                number = int(str(current_range)*repeats)
+                
+                if repeats == 1:  # Additional check for single repeats
+                    if len(set(str(number))) != 1:
+                        continue
+                
+                if begin <= number <= end:
+                    pattern_numbers.append(number)
+
+    uniqued_list = list(set(pattern_numbers))
+    uniqued_list.sort()
+    # Single digit is not repeated digit
+    filtered_list = [x for x in uniqued_list if x >= 10]
+
+    return filtered_list
+
+def has_pattern(number: int) -> bool:
+    number_str = str(number)
+    number_length = len(number_str)
+    if number_length == 1:
+        return False # One digit has not similarity pattern
+    for repeats in range(1, number_length // 2 + 1):
+        if number_length % repeats != 0:
+            continue # the pattern does not have the same number of repetitions
+        repetition = [
+            number_str[start_index*repeats:start_index*repeats+repeats]
+                for start_index in range(number_length // repeats)
+        ]
+        if len(set(repetition)) == 1:
+            return True
+    return False
+
+def find_pattern_between_ranges_brute_force(begin: int, end: int) -> list[int]:
+    pattern_numbers = []
+    for number in range(begin, end+1):
+        if has_pattern(number):
             pattern_numbers.append(number)
 
     return pattern_numbers
-
 
 if __name__ == '__main__':
     # First parameter: from (include)
@@ -34,6 +78,7 @@ if __name__ == '__main__':
 
     invalid_id = []
     for single_range in ranges:
-        invalid_id.extend(find_pattern_between_ranges(single_range[0], single_range[1]))
+        #invalid_id.extend(find_pattern_between_ranges(single_range[0], single_range[1]))
+        invalid_id.extend(find_pattern_between_ranges_brute_force(single_range[0], single_range[1]))
 
     print(sum(invalid_id))
--- Day2/test_main.py
+++ Day2/test_main.py
@@ -1,9 +1,9 @@
-from main import find_pattern_between_ranges
+from main import find_pattern_between_ranges, has_pattern
 
 def test_example():
     assert find_pattern_between_ranges(begin=11, end=22) == [11, 22]
-    assert find_pattern_between_ranges(begin=95, end=115) == [99]
-    assert find_pattern_between_ranges(begin=998, end=1012) == [1010]
+    assert find_pattern_between_ranges(begin=95, end=115) == [99, 111]
+    assert find_pattern_between_ranges(begin=998, end=1012) == [999, 1010]
     assert find_pattern_between_ranges(
         begin=1188511880,
         end=1188511890
@@ -21,12 +21,38 @@ def test_example():
         begin=38593856,
         end=38593862
     ) == [38593859]
-    assert find_pattern_between_ranges(begin=565653, end=565659) == []
+    assert find_pattern_between_ranges(
+        begin=565653,
+        end=565659
+    ) == [565656]
     assert find_pattern_between_ranges(
         begin=824824821,
         end=824824827
-    ) == []
+    ) == [824824824]
     assert find_pattern_between_ranges(
         begin=2121212118,
         end=2121212124
-    ) == []
+    ) == [2121212121]
+    
+
+def test_additional():
+    assert find_pattern_between_ranges(begin=3, end=20) == [11]
+    assert find_pattern_between_ranges(begin=62668, end=105646) == [
+        66666,
+        77777,
+        88888,
+        99999,
+        100100,
+        101010, 
+        101101,
+        102102,
+        103103,
+        104104,
+        105105
+    ]
+    assert has_pattern(3) == False
+    assert has_pattern(99) == True
+    assert has_pattern(83398339) == True
+    assert has_pattern(446443) == False
+    assert has_pattern(446446) == True
+    assert has_pattern(77777777) == True
